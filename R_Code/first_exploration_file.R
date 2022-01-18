829045efc2a4229d7e8e4a8d7b07c52b4dffdfb8
Valentin Hassler <Duplexmayer@gmail.com>
2022-01-18 01:11:43 +0100

    1. Some easy analysis and data manip. with ggplot and lme4 2. some highlighted stuff in pdf

--- R_Code/first_exploration_file.R
+++ R_Code/first_exploration_file.R
@@ -1,4 +1,34 @@
 getwd()
 data <- readRDS("data\\AIdataset.Rds")
-#MV Mechanical Ventilation
-#ICU intensive care unit
+#MV   Mechanical Ventilation
+#ICU  intensive care unit
+#PVA  Patient-ventilator asynchronies
+#SOFA measures the degree of organ dysfunction and assesses 
+      #the evolution of the patient’s severity over the ICU stay
+#AI  measures PVAs and assesses overall severity
+#### Data Changes ####
+#ai is somehow called di
+####Initial Changes####
+colnames(data)[6] <- "ai"
+age_real <- data$age
+day_real <- data$day
+data$age <- data$age - mean(data$age)
+data$day <- data$day - mean(data$day)
+#### some Plots ####
+library(ggplot2)
+ggplot(data,aes(x=day_real,y=sofa)) + stat_binhex(na.rm = T) + scale_fill_gradient(low = "blue", high = "green")
+ggplot(data,aes(x=day_real,y=sofa)) + geom_point(aes(colour = id)) +guides(colour="none")
+ggplot(data,aes(x=day_real,y=sofa)) + geom_point(aes(colour = id)) + 
+  guides(colour="none")+geom_text(mapping = aes(label = id),hjust=0, vjust=0)
+
+
+ggplot(data,aes(x=age_real,y=sofa)) + stat_binhex(na.rm = T) + scale_fill_gradient(low = "blue", high = "green")
+ggplot(data,aes(x=age_real,y=sofa)) + geom_point(aes(colour = id)) +guides(colour="none")
+ggplot(data,aes(x=age_real,y=sofa)) + geom_point(aes(colour = id)) + 
+  guides(colour="none")+geom_text(mapping = aes(label = id),hjust=0, vjust=0)
+
+####Frequentist analysis ####
+library(lme4)
+
+sofa_freq <- lmer(sofa ~ age + day +(1|id), data, REML = T)
+summary(sofa_freq)
