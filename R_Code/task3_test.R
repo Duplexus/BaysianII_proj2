5d4443d6e4ca313f90e1db73cf308b9657f348f1
1337Leo1337 <61457797+1337Leo1337@users.noreply.github.com>
2022-01-27 15:44:09 +0100

    added our predicted ai and sofa scores, some cleaning

--- R_Code/task3_test.R
+++ R_Code/task3_test.R
@@ -4,26 +4,33 @@ library("survival")
 library(nimble)
 library(lattice)
 library(dplyr)
-dataman <- readRDS("AIdataset.RDS")
+source("helpfunctions.R")
+dataman <- readRDS("../AIdataset.RDS")
+pred_sofa <- readRDS("../y_pred4_sofa.Rds")
+pred_ai <- readRDS("../pred_ai.Rds")
 data_e03 <- readRDS("Prep_data_for_ex3.rds")
-data_e03$day <- dataman$day
-data_e03$age <- dataman$age
+data_e03$day <- as.numeric(dataman$day)
+data_e03$age <- as.numeric(dataman$age)
+data_e03$sofa_pred <- as.numeric(pred_sofa)
+data_e03$ai_pred <- as.numeric(pred_ai)
+data_e03$id <- as.numeric(data_e03$id)
+data_e03$fail <- as.numeric(data_e03$fail)
 
 data_e03$fail2 <- ifelse(data_e03$fail == 2, 0, data_e03$fail)
 cens <- ifelse(data_e03$fail == 0, 30, 0)
 is.censored <- ifelse(data_e03$fail2 == 0,1,0)
 data_e03$t <- ifelse(is.censored == 1,NA,data_e03$day)
 
-model.inits <- list(list(k=3, beta0=1, beta1 = 1,beta2 = 1 ),
-                    list(k=1, beta0=10, beta1 = 10,beta2 = 10 ),
-                    list(k=0.5, beta0=20, beta1 = -10,beta2 = -15 )
+model.inits <- list(list(k=2, beta0=0.1, beta1 = 5,beta2 = 10 ),
+                    list(k=1, beta0=20, beta1 = 20,beta2 = 20 ),
+                    list(k=0.5, beta0=2, beta1 = -1,beta2 = -1 )
 )
 
-model.constants <- list( N = length(data_e03$fail), x1 = data_e03$sofa_pred,
-                         x2 = data_e03$ai_pred)
+model.constants <- list( N = length(data_e03$fail), Nsubj=length(unique(data_e03$id)),id=data_e03$id)
 
 data_e03$fail3 <- ifelse(data_e03$fail == 1, data_e03$icustaymv, 1000)
-model.data <- list(y = data_e03$t, cens=cens)
+model.data <- list(y = data_e03$t, cens=cens, x1 = data_e03$sofa_pred,
+                   x2 = data_e03$ai_pred)
 
 
 
@@ -35,7 +42,7 @@ model.function <- nimbleCode({
     y[i] ~ dweib(k, invlambda[i])
     invlambda[i] <- pow(t[i], k)
     t[i] <- exp(-h[i])
-    h[i] <- beta0 + beta1 *x1[i] + beta2 *x2[i]
+    h[i] <- beta0 + beta1 *x1[i] + beta2 *x2[i] + b0[id[i]] #b0 random intercept wrt patient
     # Distribution of future observed counts for plate i
     #predict[i]  ~ dweib(k, invlambda[i]) 
   }
@@ -45,6 +52,11 @@ model.function <- nimbleCode({
   beta0 ~ dnorm(0,0.00001)
   beta1 ~ dnorm(0,0.00001)
   beta2 ~ dnorm(0,0.00001)
+  tau_b0 <- 1/sigma2_b0
+  sigma2_b0 ~ dunif(0,1000)
+  for(z in 1:Nsubj){
+    b0[z] ~ dnorm(0,tau_b0)
+  }
 })
 
 
@@ -57,11 +69,11 @@ runjags.options(method = "rjparallel")
 weibull_e03 <- nimbleMCMC(
   code = model.function, constants = model.constants,
   data = model.data, inits = model.inits,
-  monitors = parameters, nchains = 3, niter = 20000,
-  nburnin = 3000, thin = 3, setSeed = c(1,2,3),
+  monitors = parameters, nchains = 3, niter = 100000,
+  nburnin = 20000, thin = 10, setSeed = c(1,2,3),
   samplesAsCodaMCMC = T, samples = T, 
   WAIC = T, summary=T) 
-summary(weibull_e03$samples)
+summarise_default(weibull_e03$samples)
 
 
 
